2a10edef926c13e5686cd35aa5165420f0261838
3DSA <146782672+3DSA@users.noreply.github.com>
2026-04-12 12:11:12 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count = {}
+        for s in s1:
+            count[s] = count.get(s, 0) + 1
+        l = 0
+        r = 0
+        curr = {}
+        for r in range(len(s2)):
+            if curr == count:
+                return True
+            if r-l+1 > len(s1):
+                curr[s2[l]] -= 1
+                if curr[s2[l]] == 0:
+                    curr.pop(s2[l])
+                l+=1
+            curr[s2[r]] = curr.get(s2[r], 0) + 1
+
+        return curr == count
+        
