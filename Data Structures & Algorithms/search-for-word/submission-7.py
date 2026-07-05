9f0f7eb65b9c69c1208a468269c91a3f5f4729fd
3DSA <146782672+3DSA@users.noreply.github.com>
2026-07-04 21:46:51 -0700

    Update: search-for-word - submission-7

--- Data Structures & Algorithms/search-for-word/submission-7.py
+++ Data Structures & Algorithms/search-for-word/submission-7.py
@@ -1,39 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        indices = []
+        res = False
+        def traverse(i, j, curr):
+            nonlocal res
+            if curr == len(word):
+                res = True
+                return
+            if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] != word[curr]:
+                return
+            temp = board[i][j]
+            board[i][j] = "#"
+            traverse(i+1, j, curr+1)
+            traverse(i-1,j, curr+1)
+            traverse(i, j+1, curr+1)
+            traverse(i,j-1,curr+1)
+            board[i][j] = temp
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    indices.append([i,j])
-        
-        res = False
-        def search(i, word, row, col):
-            nonlocal res
-            if board[row][col] != word[i]:
-                return
-            i+=1
-            temp = board[row][col]
-            board[row][col] = "#"
-            if i == len(word):
-                res = True
-                return
-            if row-1 >= 0:
-                search(i, word, row-1, col)
-            if row+1 < len(board):
-                search(i, word, row+1, col)
-            if col-1 >= 0:
-                search(i, word, row, col-1)
-            if col+1 < len(board[0]):
-                search(i, word, row, col+1)
-            board[row][col] = temp
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                search(0, word, i, j)
-        return res 
-            
-
-            
-
-
-
-        
+                    traverse(i, j, 0)
+                    if res:
+                        return res
+        return False
